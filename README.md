cf83cc458448f84016706ba31e80239bc3bf6e45
Scotty Franzyshen <sfranzyshen@gmail.com>
2021-06-30 09:48:13 -0700

    initial changes
    this is a modification of Ben Smith's wasm-clang demonstration to be used with wasm3 and the esp platforms

--- README.md
+++ README.md
@@ -1,41 +1,15 @@
-# Clang/LLD in WebAssembly
-
-This is the demo for my CppCon 2019 talk! I'll update with a link to the video when it is live.
-
-Thing should work, but it's still very much alpha demoware. Go to https://binji.github.io/wasm-clang to see it in action.
-
-Most of the JavaScript code can be found here. If you're interested in the implementation of the in-memory filesystem, take a look at https://github.com/binji/llvm-project/tree/master/binji.
+# Running Clang/LLD in the browser
 
 # Directory structure
 
-## Shared utilities
-
-- `clang`: clang compiler, compiled to wasm w/ WASI
-- `lld`: lld linker, compiled to wasm w/ WASI
-- `main`.css
-- `memfs`: WASI implementation of in-memory filesystem,
-- `shared.js`: shared utilities for all tools (web and d8)
-- `shared_web.js`: shared utilities for all web tools (asm.html, index.html, etc.)
-- `sysroot.tar`: C++ standard headers and libraries
+- `clang.wasm`: clang compiler, compiled to wasm w/ WASI
+- `lld.wasm`: lld linker, compiled to wasm w/ WASI
+- `memfs.wasm`: wasm w/ WASI implementation of in-memory filesystem,
+- `index.html`
+- `index.css`
+- `index.js`
 - `service_worker.js`: Service worker used by all web pages
 - `worker.js`: Dedicated work used to compile/run
+- `sysroot.tar`: C++ standard headers and libraries
 
-## Assemble 6502 code
 
-- `6502.html`
-- `6502.js`
-- `vasm6502_oldstyle`: [vasm assembler](http://sun.hasenbraten.de/vasm/) compiled to wasm w/ WASI
-
-## Compile C++ code to x86/wasm assembly
-
-- `asm.html`
-- `asm.js`
-
-## Compile C++ code in d8 (v8's console shell)
-
-- `d8.js`
-
-## Compile and run C++ code
-
-- `index.html`
-- `web.js`
--- web.js
+++ web.js
@@ -17,27 +17,92 @@
 const LAYOUT_CONFIG_KEY = 'layoutConfig';
 
 const initialProgram =
-`#include <canvas.h>
-#include <stdint.h>
+`#include <arduino_api.h>
 
-const int w = 1000;
-const int h = 800;
-Canvas c{w, h};
-ImageData image{w, h};
+int patternCurrent = 0;              // Current Pattern Number
 
-int main() {
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            image.data[y * w + x] = RGB(x | y, 0, 0);
-        }
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j <= 255; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < numPixels(); i=i+3) {
+        setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < numPixels(); i=i+3) {
+        setPixelColor(i+q, 0);        //turn every third pixel off
+      }
     }
-    image.commit();
-    c.putImageData(image, 0, 0);
+  }
+}
 
-    const char* msg = "x | y";
-    c.setFillStyle("white");
-    c.setFont("bold 200px sans");
-    c.fillText(msg, (w - c.measureText(msg)) / 2, (h + 100) / 2);
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<numPixels(); i++) {
+    setPixelColor(i, c);
+    show();
+    delay(wait);
+  }
+}
+
+//Rainbow effect
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<=255; j++) {
+    for(i=0; i<numPixels(); i++) {
+      setPixelColor(i, Wheel((i+j) & 255));
+    }
+    show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < numPixels(); i=i+3) {
+        setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < numPixels(); i=i+3) {
+        setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// setup()
+void setup() {
+    println("\nstrandtest ... start");
+}
+
+// loop()
+void loop() {
+    patternCurrent++;
+
+    // Some example procedures showing how to display to the pixels:
+
+    if(patternCurrent == 8) { println("theaterChaseRainbow"); theaterChaseRainbow(50); }
+    else if(patternCurrent == 7) { println("rainbow"); rainbow(20); }
+    else if(patternCurrent == 6) { println("theaterChase Blue"); theaterChase(Color(0, 0, 127), 50); }      // Blue
+    else if(patternCurrent == 5) { println("theaterChase Red"); theaterChase(Color(127, 0, 0), 50); }       // Red
+    else if(patternCurrent == 4) { println("theaterChase White"); theaterChase(Color(127, 127, 127), 50); } // White
+    else if(patternCurrent == 3) { println("colorWipe Blue"); colorWipe(Color(0, 0, 255), 50); }            // Blue
+    else if(patternCurrent == 2) { println("colorWipe Green"); colorWipe(Color(0, 255, 0), 50); }           // Green
+    else if(patternCurrent == 1) { println("colorWipe Red"); colorWipe(Color(255, 0, 0), 50); }             // Red
+    if(patternCurrent >= 8) {
+    	patternCurrent = 0;
+	println("strandtest ... loop");
+    }
 }
 `;
 
